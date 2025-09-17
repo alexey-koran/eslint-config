d95e36c89a62b5530442f17f34b7b26e65b12dfa
Alexey Koran <alexey.koran@gmail.com>
2025-09-17 16:58:21 +0400

    fix: prettier config import

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,7 @@
-import externalConfig from '@alexey-koran/prettier-config';
+import { mjs } from '@alexey-koran/prettier-config';
 
 /** @type {import('prettier').Config} */
 
 export default {
-  ...externalConfig,
+  ...mjs,
 };
