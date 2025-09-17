142ace2daab7308c1fe2268d4328447427f46d38
Alexey Koran <alexey.koran@gmail.com>
2025-09-17 20:52:01 +0400

    chore: add pnpm-workspace, tsconfig to .npmignore

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,12 @@ import { type Linter } from 'eslint';
 export const shareableConfig: Linter.Config[];
 
 export const baseConfig: Linter.Config;
+
 export const javaScriptConfig: Linter.Config;
+
 export const typeScriptConfig: Linter.Config;
 export const typeScriptBaseConfig: Linter.Config;
+
 export const typeScriptReactConfig: Linter.Config;
 export const reactConfig: Linter.Config;
 
